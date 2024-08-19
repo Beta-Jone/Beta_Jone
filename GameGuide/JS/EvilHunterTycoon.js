46ad3d64a193e9388633d86f147d682110f26846
Beta-Jone <108645481+Beta-Jone@users.noreply.github.com>
2024-08-19 11:08:55 +0630

    Add files via upload

--- GameGuide/JS/EvilHunterTycoon.js
+++ GameGuide/JS/EvilHunterTycoon.js
@@ -4,6 +4,34 @@ function OnLoad() {
     SetIndex();
     SetPageData();
 }
+//--------------------------------------------------------------------------------------------------------------------Include HTML
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain attribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
+};
 //--------------------------------------------------------------------------------------------------------------------Nevigation
 function NavigationBar(let) {
     if (let == "Open") {
@@ -16,6 +44,16 @@ function NavigationBar(let) {
     }
 }
 function NavigationManagement(let) {
+    if (let == "Hunter") {
+        if (document.getElementById("Hunter1").style.display == "block") {
+            document.getElementById("Hunter1").style.display = "none";
+            document.getElementById("Hunter2").innerHTML = "=>";
+        }
+        else if (document.getElementById("Hunter1").style.display == "none") {
+            document.getElementById("Hunter1").style.display = "block";
+            document.getElementById("Hunter2").innerHTML = "<=";
+        }
+    }
     if (let == "Index") {
         if (document.getElementById("Index1").style.display == "block") {
             document.getElementById("Index1").style.display = "none";
